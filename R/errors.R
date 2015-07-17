90c47ef8f5bbf06cb16e4af986f93892d8ba6c1e
paulhendricks <paul.hendricks.2013@owu.edu>
2015-07-17 19:59:34 +0000

    update with rmse

--- R/errors.R
+++ R/errors.R
@@ -79,3 +79,23 @@ mape <- function(actual, forecast, na.rm = TRUE, ...) {
   return(mean(ape(actual, forecast), na.rm = na.rm, ...))
 }
 
+#' Calculate root mean squared error.
+#'
+#' \code{rmse} takes actual and forecast numeric vectors and returns a
+#' numeric vector where forecast is subtracted from the actual,
+#' the errors are then squared, the average of those squared error is
+#' taken, and lastly, the root taken of that mean squared error value.
+#'
+#' @param actual a numeric vector of actuals.
+#' @param na.rm a logical value indicating whether \code{NA} values should be stripped before the computation proceeds.
+#' @param ... additional arguments to be passed to mean()
+#' @param forecast a numeric vector of forecasts.
+#' @return  a numeric vector of length one: the mean of of absolute percent errors.
+#' @examples
+#' # Examples
+#' rmse(1:100, 100:1)
+#' @export
+rmse <- function(actual, forecast, na.rm = TRUE, ...) {
+  return(sqrt(mean(e(actual, forecast) ^ 2, na.rm = na.rm, ...)))
+}
+
