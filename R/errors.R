2b70cb9bfc015085ff7b1ff24986ad94e56fd3bb
paulhendricks <paul.hendricks.2013@owu.edu>
2015-07-20 13:43:25 +0000

    added mean absolute error/deviation

--- R/errors.R
+++ R/errors.R
@@ -27,6 +27,26 @@ e <- function(actual, forecast) return(actual - forecast)
 #' @export
 ae <- function(actual, forecast) return(abs(e(actual, forecast)))
 
+#' Calculate mean absolute error between actual and forecast.
+#'
+#' \code{ae} takes actual and forecast numeric vectors and returns a
+#' numeric vector where forecast is subtracted from the actual, the
+#' absolute value is taken of those errors, and lastly, the mean is
+#' taken of those absolute erros.
+#'
+#' @param actual a numeric vector of actuals.
+#' @param forecast a numeric vector of forecasts.
+#' @param na.rm a logical value indicating whether \code{NA} values should be stripped before the computation proceeds.
+#' @param ... additional arguments to be passed to mean()
+#' @return  a numeric vector of length one representing the mean of absolute errors.
+#' @examples
+#' # Examples
+#' mae(1:100, 100:1)
+#' @export
+mae <- function(actual, forecast, na.rm = TRUE, ...) {
+  return(mean(ae(actual, forecast), na.rm = na.rm, ...))
+}
+
 #' Calculate percent error between actual and forecast.
 #'
 #' \code{pe} takes actual and forecast numeric vectors and returns a
@@ -67,9 +87,9 @@ ape <- function(actual, forecast) return(abs(pe(actual, forecast)))
 #' the mean of those absolute percent errors are taken.
 #'
 #' @param actual a numeric vector of actuals.
+#' @param forecast a numeric vector of forecasts.
 #' @param na.rm a logical value indicating whether \code{NA} values should be stripped before the computation proceeds.
 #' @param ... additional arguments to be passed to mean()
-#' @param forecast a numeric vector of forecasts.
 #' @return  a numeric vector of length one: the mean of of absolute percent errors.
 #' @examples
 #' # Examples
@@ -87,9 +107,9 @@ mape <- function(actual, forecast, na.rm = TRUE, ...) {
 #' taken, and lastly, the root taken of that mean squared error value.
 #'
 #' @param actual a numeric vector of actuals.
+#' @param forecast a numeric vector of forecasts.
 #' @param na.rm a logical value indicating whether \code{NA} values should be stripped before the computation proceeds.
 #' @param ... additional arguments to be passed to mean()
-#' @param forecast a numeric vector of forecasts.
 #' @return  a numeric vector of length one: the mean of of absolute percent errors.
 #' @examples
 #' # Examples
