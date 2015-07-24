31f4bf09f16b30982f20fb25644aa42b14a14013
paulhendricks <paul.hendricks.2013@owu.edu>
2015-07-24 14:48:37 +0000

    updated test

--- tests/testthat/test-mape.R
+++ tests/testthat/test-mape.R
@@ -5,6 +5,8 @@ n <- 10000
 x <- 1:n
 test_that("mape() produces correct output.", {
   expect_equal(mape(x, x), 0)
+  expect_equal(mape(0, 1), Inf)
+  expect_equal(mape(1, 0), 1)
 })
 
 test_that("mape() produces correct output types.", {
