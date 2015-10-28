87da10c8807bb8aabee96a13560b1f2da240ed4c
Paul Hendricks <paul.hendricks.2013@owu.edu>
2015-10-28 14:27:38 -0400

    added function to calculate mean squared error

--- R/errors.R
+++ R/errors.R
@@ -1,6 +1,6 @@
 #' Calculate area under the curve between actual and forecast.
 #'
-#' \code{auc} does something.
+#' \code{auc} calculates the area under the curve.
 #'
 #' @param actual A binary numeric vector of actuals.
 #' @param forecast A binary numeric vector of forecasts.
@@ -20,20 +20,19 @@ auc <- function(actual, forecast) {
   auc
 }
 
-#' Title.
+#' Calculate mean squared error between actual and forecast.
 #'
-#' \code{function_name} does something.
+#' \code{mse} calculates the mean squared error between actual and forecast.
 #'
 #' @param actual A numeric vector of actuals.
 #' @param forecast A numeric vector of forecasts.
 #' @return  A numeric vector of length one.
 #' @examples
 #' # Examples
-#'
+#' mse(1:100, 100:1)
 #' @export
 mse <- function(actual, forecast) {
-  warning("This function is still under development.")
-  return(1L)
+  return(mean((actual - forecast) ^ 2))
 }
 
 #' Title.
