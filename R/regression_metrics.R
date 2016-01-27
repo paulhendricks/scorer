b4e84fa20555794e0e51da6dc07e22400546e6be
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-01-27 14:19:44 -0500

    reordered functions

--- R/regression_metrics.R
+++ R/regression_metrics.R
@@ -39,6 +39,17 @@ median_absolute_percent_error <- function(y_true, y_pred) {
   return(median(absolute_percent_error(y_true = y_true, y_pred = y_pred)))
 }
 
+#' Calculate mean absolute scaled error regression loss.
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  A numeric vector of length one.
+#' @export
+mean_absolute_scaled_error <- function(y_true, y_pred) {
+  message("Function not implemented yet.")
+  return(1L)
+}
+
 #' Calculate R^2 (coefficient of determination) regression score function.
 #'
 #' @param y_true Ground truth (correct) target values.
@@ -56,17 +67,6 @@ r2_score <- function(y_true, y_pred) {
 #' @return  A numeric vector of length one.
 #' @export
 explained_variance_score <- function(y_true, y_pred) {
-  message("Function not implemented yet.")
-  return(1L)
-}
-
-#' Calculate explained variance regression score function.
-#'
-#' @param y_true Ground truth (correct) target values.
-#' @param y_pred Estimated target values.
-#' @return  A numeric vector of length one.
-#' @export
-mean_absolute_scaled_error <- function(y_true, y_pred) {
   message("Function not implemented yet.")
   return(1L)
 }
