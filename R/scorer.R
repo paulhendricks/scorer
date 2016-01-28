dd7ac844974b1c0908fbeee9a3f0a76f52a43ad8
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-01-28 15:58:49 -0500

    updated description and docuemntation

--- R/scorer.R
+++ R/scorer.R
@@ -1,4 +1,4 @@
-#' scorer: Quickly Score Models.
+#' scorer: Quickly Score Models in Data Science and Machine Learning.
 #'
 #' A set of tools for quickly scoring models in data science and machine
 #' learning. This toolset is written in C++ for blazing fast performance.
