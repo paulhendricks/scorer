68d0ab8b5282ca733b4cc529636d315a0894e0c4
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-01-28 13:43:18 -0500

    added test template for classification_metrics

---
+++ tests/testthat/test-classification_metrics.R
@@ -0,0 +1,14 @@
+library(scorer)
+context("classification metrics")
+
+test_that("Produces correct output.", {
+  expect_equal(1L, 1L)
+})
+
+test_that("Produces correct output classes and types.", {
+  expect_equal(1L, 1L)
+})
+
+test_that("Produces the correct messages, warnings, and errors.", {
+  expect_equal(1L, 1L)
+})
