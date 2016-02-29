52ff8c23ed706cef700063fbc2bb40697b1149a7
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-02-29 11:05:17 -0500

    starting to add classification functions

--- R/classification.R
+++ R/classification.R
@@ -6,7 +6,11 @@
 #' @family classification metrics
 #' @export
 auc_score <- function(y_true, y_pred) {
-  return(1L)
+  r <- rank(y_pred)
+  n_pos <- sum(y_true == 1)
+  n_neg <- length(y_true) - n_pos
+  auc <- (sum(r[y_true == 1]) - n_pos * (n_pos + 1) /2) / (n_pos * n_neg)
+  return(auc)
 }
 
 #' Calculate accuracy score.
