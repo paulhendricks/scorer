a20b6f98589d13922411b9bff1c1249805135a02
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-02-29 11:12:50 -0500

    added templates for classification metrics

--- R/classification.R
+++ R/classification.R
@@ -23,3 +23,201 @@ auc_score <- function(y_true, y_pred) {
 accuracy_score <- function(y_true, y_pred) {
   return((y_true == y_pred) / length(y_true))
 }
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+average_precision_score <- function(y_true, y_pred) {
+  return(TRUE)
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+brier_score <- function(y_true, y_pred) {
+  return(TRUE)
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+classification_report <- function(y_true, y_pred) {
+  return(TRUE)
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+confusion_matrix <- function(y_true, y_pred) {
+  return(TRUE)
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+f1_score <- function(y_true, y_pred) {
+  return(TRUE)
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+fbeta_score <- function(y_true, y_pred) {
+  return(TRUE)
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+hamming_loss <- function(y_true, y_pred) {
+  return(TRUE)
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+hinge_loss <- function(y_true, y_pred) {
+  return(TRUE)
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+jaccard_similarity_score <- function(y_true, y_pred) {
+  return(TRUE)
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+log_loss <- function(y_true, y_pred) {
+  return(TRUE)
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+matthews_corrcoef <- function(y_true, y_pred) {
+  return(TRUE)
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+precision_recall_curve <- function(y_true, y_pred) {
+  return(TRUE)
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+precision_recall_fscore_support <- function(y_true, y_pred) {
+  return(TRUE)
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+precision_score <- function(y_true, y_pred) {
+  return(TRUE)
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+recall_score <- function(y_true, y_pred) {
+  return(TRUE)
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+roc_auc_score <- function(y_true, y_pred) {
+  return(TRUE)
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+roc_curve <- function(y_true, y_pred) {
+  return(TRUE)
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+zero_one_loss <- function(y_true, y_pred) {
+  return(TRUE)
+}
