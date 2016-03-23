8401a59602d87bc1491d027394450b27dc138408
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-03-23 15:58:13 -0400

    added classification metrics

--- R/classification.R
+++ R/classification.R
@@ -1,223 +1,329 @@
-#' Calculate area under the curve.
+#' TO BE EDITED
 #'
 #' @param y_true Ground truth (correct) target values.
 #' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
+#' @  return  TO BE EDITED.
 #' @family classification metrics
 #' @export
-auc_score <- function(y_true, y_pred) {
-  r <- rank(y_pred)
-  n_pos <- sum(y_true == 1)
-  n_neg <- length(y_true) - n_pos
-  auc <- (sum(r[y_true == 1]) - n_pos * (n_pos + 1) / 2) / (n_pos * n_neg)
-  return(auc)
+total_population <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(length(y_true))
 }
 
-#' Calculate accuracy score.
+#' TO BE EDITED
 #'
 #' @param y_true Ground truth (correct) target values.
 #' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+true_positive <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(sum((y_true == y_pred) & (y_true == 1)))
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+true_negative <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(sum((y_true == y_pred) & (y_true == 0)))
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+false_positive <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(sum(!(y_true == y_pred) & (y_true == 1)))
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+false_negative <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(sum(!(y_true == y_pred) & (y_true == 0)))
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+condition_positive <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(sum(y_true == 1))
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+condition_negative <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(sum(y_true == 0))
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+predicted_condition_positive <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(sum(y_true == 1))
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+predicted_condition_negative <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(sum(y_pred == 0))
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
 #' @family classification metrics
 #' @export
 accuracy_score <- function(y_true, y_pred) {
-  return((y_true == y_pred) / length(y_true))
+  check_args(y_true, y_pred)
+  correct <- true_positive(y_true, y_pred) + true_negative(y_true, y_pred)
+  return(correct / total_population(y_true, y_pred))
 }
 
 #' TO BE EDITED
 #'
 #' @param y_true Ground truth (correct) target values.
 #' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
+#' @  return  TO BE EDITED.
 #' @family classification metrics
 #' @export
-average_precision_score <- function(y_true, y_pred) {
-  return(TRUE)
-}
+accuracy <- accuracy_score
 
 #' TO BE EDITED
 #'
 #' @param y_true Ground truth (correct) target values.
 #' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
+#' @  return  TO BE EDITED.
 #' @family classification metrics
 #' @export
-brier_score <- function(y_true, y_pred) {
-  return(TRUE)
+prevalence <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(condition_positive(y_true, y_pred) / total_population(y_true, y_pred))
 }
 
 #' TO BE EDITED
 #'
 #' @param y_true Ground truth (correct) target values.
 #' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
+#' @  return  TO BE EDITED.
 #' @family classification metrics
 #' @export
-classification_report <- function(y_true, y_pred) {
-  return(TRUE)
+positive_predictive_value <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(true_positive(y_true, y_pred) / predicted_condition_positive(y_true, y_pred))
 }
 
 #' TO BE EDITED
 #'
 #' @param y_true Ground truth (correct) target values.
 #' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
+#' @  return  TO BE EDITED.
 #' @family classification metrics
 #' @export
-confusion_matrix <- function(y_true, y_pred) {
-  return(TRUE)
+precision <- positive_predictive_value
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+false_discovery_rate <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(false_positive(y_true, y_pred) / predicted_condition_positive(y_true, y_pred))
 }
 
 #' TO BE EDITED
 #'
 #' @param y_true Ground truth (correct) target values.
 #' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+negative_predictive_value <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(true_negative(y_true, y_pred) / predicted_condition_negative(y_true, y_pred))
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+false_omission_rate <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(false_negative(y_true, y_pred) / predicted_condition_negative(y_true, y_pred))
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+true_positive_rate <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(true_positive(y_true, y_pred) / condition_positive(y_true, y_pred))
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+
+sensitivity <- true_positive_rate
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+recall <- true_positive_rate
+
+false_positive_rate <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(false_positive(y_true, y_pred) / condition_negative(y_true, y_pred))
+}
+
+false_negative_rate <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(false_negative(y_true, y_pred) / condition_positive(y_true, y_pred))
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+true_negative_rate <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(true_negative(y_true, y_pred) / condition_negative(y_true, y_pred))
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+specificity <- true_negative_rate
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+positive_likelihood_ratio <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(true_positive_rate(y_true, y_pred) / false_positive_rate(y_true, y_pred))
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+negative_likelihood_ratio <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(false_negative_rate(y_true, y_pred) / true_negative_rate(y_true, y_pred))
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
+#' @family classification metrics
+#' @export
+diagnostic_odds_ratio <- function(y_true, y_pred) {
+  check_args(y_true, y_pred)
+  return(positive_likelihood_ratio(y_true, y_pred) / negative_likelihood_ratio(y_true, y_pred))
+}
+
+#' TO BE EDITED
+#'
+#' @param y_true Ground truth (correct) target values.
+#' @param y_pred Estimated target values.
+#' @  return  TO BE EDITED.
 #' @family classification metrics
 #' @export
 f1_score <- function(y_true, y_pred) {
-  return(TRUE)
+  check_args(y_true, y_pred)
+  numerator <- 2 * true_positive(y_true, y_pred)
+  denominator <- 2 * true_positive(y_true, y_pred) + false_positive(y_true, y_pred) + false_negative(y_true, y_pred)
+  return(numerator / denominator)
 }
 
 #' TO BE EDITED
 #'
 #' @param y_true Ground truth (correct) target values.
 #' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
-#' @family classification metrics
-#' @export
-fbeta_score <- function(y_true, y_pred) {
-  return(TRUE)
-}
-
-#' TO BE EDITED
-#'
-#' @param y_true Ground truth (correct) target values.
-#' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
-#' @family classification metrics
-#' @export
-hamming_loss <- function(y_true, y_pred) {
-  return(TRUE)
-}
-
-#' TO BE EDITED
-#'
-#' @param y_true Ground truth (correct) target values.
-#' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
-#' @family classification metrics
-#' @export
-hinge_loss <- function(y_true, y_pred) {
-  return(TRUE)
-}
-
-#' TO BE EDITED
-#'
-#' @param y_true Ground truth (correct) target values.
-#' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
-#' @family classification metrics
-#' @export
-jaccard_similarity_score <- function(y_true, y_pred) {
-  return(TRUE)
-}
-
-#' TO BE EDITED
-#'
-#' @param y_true Ground truth (correct) target values.
-#' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
-#' @family classification metrics
-#' @export
-log_loss <- function(y_true, y_pred) {
-  return(TRUE)
-}
-
-#' TO BE EDITED
-#'
-#' @param y_true Ground truth (correct) target values.
-#' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
+#' @  return  TO BE EDITED.
 #' @family classification metrics
 #' @export
 matthews_corrcoef <- function(y_true, y_pred) {
-  return(TRUE)
-}
-
-#' TO BE EDITED
-#'
-#' @param y_true Ground truth (correct) target values.
-#' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
-#' @family classification metrics
-#' @export
-precision_recall_curve <- function(y_true, y_pred) {
-  return(TRUE)
-}
-
-#' TO BE EDITED
-#'
-#' @param y_true Ground truth (correct) target values.
-#' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
-#' @family classification metrics
-#' @export
-precision_recall_fscore_support <- function(y_true, y_pred) {
-  return(TRUE)
-}
-
-#' TO BE EDITED
-#'
-#' @param y_true Ground truth (correct) target values.
-#' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
-#' @family classification metrics
-#' @export
-precision_score <- function(y_true, y_pred) {
-  return(TRUE)
-}
-
-#' TO BE EDITED
-#'
-#' @param y_true Ground truth (correct) target values.
-#' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
-#' @family classification metrics
-#' @export
-recall_score <- function(y_true, y_pred) {
-  return(TRUE)
-}
-
-#' TO BE EDITED
-#'
-#' @param y_true Ground truth (correct) target values.
-#' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
-#' @family classification metrics
-#' @export
-roc_auc_score <- function(y_true, y_pred) {
-  return(TRUE)
-}
-
-#' TO BE EDITED
-#'
-#' @param y_true Ground truth (correct) target values.
-#' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
-#' @family classification metrics
-#' @export
-roc_curve <- function(y_true, y_pred) {
-  return(TRUE)
-}
-
-#' TO BE EDITED
-#'
-#' @param y_true Ground truth (correct) target values.
-#' @param y_pred Estimated target values.
-#' @return  TO BE EDITED.
-#' @family classification metrics
-#' @export
-zero_one_loss <- function(y_true, y_pred) {
-  return(TRUE)
+  check_args(y_true, y_pred)
+  numerator_1 <- true_positive(y_true, y_pred) * true_negative(y_true, y_pred)
+  numerator_2 <- false_positive(y_true, y_pred) * false_negative(y_true, y_pred)
+  numerator <- numerator_1 - numerator_2
+  denominator_1 <- true_positive(y_true, y_pred) + false_positive(y_true, y_pred)
+  denominator_2 <- true_positive(y_true, y_pred) + false_negative(y_true, y_pred)
+  denominator_3 <- true_negative(y_true, y_pred) + false_positive(y_true, y_pred)
+  denominator_4 <- true_negative(y_true, y_pred) + false_negative(y_true, y_pred)
+  denominator <- denominator_1 * denominator_2 * denominator_3 * denominator_4
+  return(numerator / (denominator ^ 0.5))
 }
--- tests/testthat/test-classification.R
+++ tests/testthat/test-classification.R
@@ -1,5 +1,5 @@
 library(scorer)
-context("regression")
+context("classification")
 
 # absolute_error
 # percent_error
@@ -25,70 +25,3 @@ context("regression")
 # explained_variance_score
 # unexplained_variance_score
 # r2_score
-
-n <- 10000
-x <- 1:n
-
-test_that("absolute_error() produces correct output.", {
-  expect_equal(absolute_error(x, x), rep(0, n))
-  expect_equal(absolute_error(0, 1), 1)
-  expect_equal(absolute_error(1, 0), 1)
-  expect_equal(absolute_error(FALSE, TRUE), 1)
-  expect_equal(absolute_error(TRUE, FALSE), 1)
-})
-
-test_that("absolute_error() produces correct output classes and types.", {
-  expect_is(absolute_error(runif(n), runif(n)), "numeric")
-})
-
-test_that("absolute_error() raises correct messages, warnings, and errors.", {
-  expect_error(absolute_error("a", "b"))
-})
-
-test_that("percent_error() produces correct output.", {
-  expect_equal(percent_error(x, x), rep(0, n))
-  expect_equal(percent_error(0, 1), -Inf)
-  expect_equal(percent_error(1, 0), 1)
-  expect_equal(percent_error(FALSE, TRUE), -Inf)
-  expect_equal(percent_error(TRUE, FALSE), 1)
-})
-
-test_that("percent_error() produces correct output classes and types.", {
-  expect_is(percent_error(runif(n), runif(n)), "numeric")
-})
-
-test_that("percent_error() raises correct messages, warnings, and errors.", {
-  expect_error(percent_error("a", "b"))
-})
-
-test_that("log_error() produces correct output.", {
-  expect_equal(log_error(2 * exp(1), exp(1)), 1)
-  expect_equal(log_error(0, 1), NaN)
-  expect_equal(log_error(1, 0), 0)
-  expect_equal(log_error(FALSE, TRUE), NaN)
-  expect_equal(log_error(TRUE, FALSE), 0)
-})
-
-test_that("log_error() produces correct output classes and types.", {
-  expect_is(log_error(runif(n), runif(n)), "numeric")
-})
-
-test_that("log_error() raises correct messages, warnings, and errors.", {
-  expect_error(log_error("a", "b"))
-})
-
-test_that("squared_error() produces correct output.", {
-  expect_equal(squared_error(x, x), rep(0, n))
-  expect_equal(squared_error(0, 1), 1)
-  expect_equal(squared_error(1, 0), 1)
-  expect_equal(squared_error(FALSE, TRUE), 1)
-  expect_equal(squared_error(TRUE, FALSE), 1)
-})
-
-test_that("squared_error() produces correct output classes and types.", {
-  expect_is(squared_error(runif(n), runif(n)), "numeric")
-})
-
-test_that("squared_error() raises correct messages, warnings, and errors.", {
-  expect_error(squared_error("a", "b"))
-})
